36ab335bf2d58a806ef886b301a92584ac1e3d06
noobkyle <kadangotinaishe5@gmail.com>
2024-06-26 22:25:11 -0500

    Adding onto main form

--- Inventory Management System/Form1.cs
+++ Inventory Management System/Form1.cs
@@ -5,11 +5,55 @@ namespace Inventory_Management_System
 		public Form1()
 		{
 			InitializeComponent();
+
+			// Configure data grid 1
+			Inventory.SelectedPartIndex = -1;
+			dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+			dataGridView1.DefaultCellStyle.SelectionForeColor = dataGridView1.DefaultCellStyle.ForeColor;
+			dataGridView1.RowHeadersVisible = false;
+			dataGridView1.DataSource = Inventory.Parts;
+
+			// Configure data grid 2
+			Inventory.SelectedProductIndex = -1;
+			dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+			dataGridView2.DefaultCellStyle.SelectionForeColor = dataGridView2.DefaultCellStyle.ForeColor;
+			dataGridView2.RowHeadersVisible = false;
+			dataGridView2.DataSource = Inventory.Products;
+
 		}
+
+		// Set INDEXES
+		private void SetSelectedPartIndex()
+		{
+			if (dataGridView1.SelectedRows.Count != 0)
+			{
+				DataGridViewRow row = this.dataGridView1.SelectedRows[0];
+				Inventory.SelectedPartIndex = row.Index;
+			}
+			else
+			{
+				Inventory.SelectedPartIndex = -1;
+			}
+		}
+
+		private void SetSelectedProductIndex() {
+			if (dataGridView2.SelectedRows.Count != 0)
+			{
+				DataGridViewRow row = dataGridView2.SelectedRows[0];
+				Inventory.SelectedProductIndex = row.Index;
+			}
+			else
+			{
+				Inventory.SelectedProductIndex = -1;
+			}
+			}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
+			DataGridViewSelectedCellCollection selectedCells = dataGridView1.SelectedCells;
 
+			dataGridView1.ClearSelection();
+			dataGridView2.ClearSelection();
 		}
 
 		private void label1_Click(object sender, EventArgs e)
@@ -39,7 +83,7 @@ namespace Inventory_Management_System
 
 		private void button3_Click(object sender, EventArgs e)
 		{
-
+			this.Close();
 		}
 
 		private void button4_Click(object sender, EventArgs e)
--- Inventory Management System/Program.cs
+++ Inventory Management System/Program.cs
@@ -8,8 +8,42 @@ namespace Inventory_Management_System
 		[STAThread]
 		static void Main()
 		{
-			// To customize application configuration such as set high DPI settings or default font,
-			// see https://aka.ms/applicationconfiguration.
+			Inhouse wheel = new Inhouse("Wheel", 18.99M, 20, 5, 25, 120);
+			Inhouse pedal = new Inhouse("Pedal", 8.22M, 11, 5, 25, 121);
+			Outsourced chain = new Outsourced("Chain", 8.33M, 12, 5, 25, "Everlast");
+			Outsourced seat = new Outsourced("Seat", 4.55M, 15, 2, 15, "Everlast");
+
+			Product redbicycle = new Product("Red Bicycle", 11.44M, 15, 1, 25);
+			Product yellowbicycle = new Product("Yellow Bicycle", 9.66M, 19, 1, 20);
+			Product bluebicycle = new Product("Blue Bicycle", 12.77M, 5, 1, 25);
+
+			Inventory.Parts.Add(wheel);
+			Inventory.Parts.Add(pedal);
+			Inventory.Parts.Add(chain);
+			Inventory.Parts.Add(seat);
+
+			Inventory.Products.Add(redbicycle);
+			Inventory.Products.Add(yellowbicycle);
+			Inventory.Products.Add(bluebicycle);
+
+			redbicycle.AssociatedParts.Add(wheel);
+			redbicycle.AssociatedParts.Add(pedal);
+			redbicycle.AssociatedParts.Add(chain);
+			redbicycle.AssociatedParts.Add(seat);
+
+			yellowbicycle.AssociatedParts.Add(wheel);
+			yellowbicycle.AssociatedParts.Add(pedal);
+			yellowbicycle.AssociatedParts.Add(chain);
+			yellowbicycle.AssociatedParts.Add(seat);
+
+			bluebicycle.AssociatedParts.Add(wheel);
+			bluebicycle.AssociatedParts.Add(pedal);
+			bluebicycle.AssociatedParts.Add(chain);
+			bluebicycle.AssociatedParts.Add(seat);
+
+
+			
+			Application.EnableVisualStyles();
 			ApplicationConfiguration.Initialize();
 			Application.Run(new Form1());
 		}
